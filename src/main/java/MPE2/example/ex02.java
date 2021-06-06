13a5f82e49ccbbd86a038c13b0470407d9b490b6
Deedo67 <70287389+Deedo67@users.noreply.github.com>
2021-06-06 17:50:44 -0400

    Initial commit

---
+++ src/main/java/MPE2/example/ex02.java
@@ -0,0 +1,34 @@
+package MPE2.example;
+
+import java.util.Scanner;
+
+/*
+ *  UCF COP3330 Summer 2021 Assignment 2 Solution
+ *  Copyright 2021 Derek Gilberti
+ */
+
+//class begin
+public class ex02 {
+    //main method begin
+    public static void main(String[] args) {
+        //Scanner
+        Scanner in = new Scanner(System.in);
+
+        //Input
+        System.out.print("What is the input string? ");
+        String name = in.nextLine();
+
+        //Counter
+        int count = 0;
+        for(int i = 0; i < name.length(); i++) {
+            if(name.charAt(i) != ' ')
+                count++;
+        }
+
+        String outputString = name + " has " + count + " characters.";
+
+        System.out.println(outputString);
+    }
+    //main method end
+}
+//class end
